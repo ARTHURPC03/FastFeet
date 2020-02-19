23ca14e627a85d9473da9adf1b09f9c7c337d15d
ARTHUR PC <arthurpimetelcordeiro@gmail.com>
2020-02-19 18:47:31 -0300

    a

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  username: 'postgres',
+  password: 'docker',
+  database: 'fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
